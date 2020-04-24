216c3e95a00dab94db3cc3056eccee9d3984e7e8
kkatayama <katayama@udel.edu>
2020-04-24 03:39:04 -0400

    update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-    
+
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
@@ -23,22 +23,26 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        logging.info(f"{bcolors.OKBLUE}GET {bcolors.ENDC} request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        message = "{}GET {} request,\nPath: {}\nHeaders:\n{}\n".format(bcolors.OKBLUE, bcolors.ENDC, self.path, self.headers)
+        logging.info(message)
+        print(message)
+
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info(f"{bcolors.OKGREEN}POST {bcolors.ENDC} request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        message = "{}POST {} request,\nPath: {}\nHeaders:\n{}\n\nBody:\n{}\n".format(bcolors.OKGREEN, bcolors.ENDC, self.path, self.headers, post_data.decode('utf-8')
+        logging.info(message)
+        print(message)
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(filename='zoom_meeting.log', filemode='w', level=logging.INFO)
-    
+
     for i in range(50):
         try :
             server_address = ('', port)
@@ -46,18 +50,23 @@ def run(server_class=HTTPServer, handler_class=S, port=8080):
             break
         except OSError:
             port += 1
-            logging.info(f"{bcolors.WARNING}PORT " + str(port-1) + " is unavalible. Trying PORT " + str(port) + f"{bcolors.ENDC}")
-    
-    logging.info(f'{bcolors.OKGREEN}Starting HTTP SERVER at PORT ' + str(port) + f'{bcolors.ENDC}')
+            message = f"{bcolors.WARNING}PORT " + str(port-1) + " is unavalible. Trying PORT " + str(port) + f"{bcolors.ENDC}"
+            print(message)
+            logging.info(message)
+
+    message = f'{bcolors.OKGREEN}Starting HTTP SERVER at PORT ' + str(port) + f'{bcolors.ENDC}'
+    print(message)
+    logging.info(message)
+
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
 
-
-        logging.info(f'port '+str(port-1)+' is taken. Trying port ' + str(port))
     httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    message = 'Stopping httpd...\n'
+    print(message)
+    logging.info(message)
 
 if __name__ == '__main__':
     from sys import argv
